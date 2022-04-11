3cbe93205dd01a960f8d2a5ff6e48dd896a092cc
HunterM7 <tonym7@yandex.ru>
2022-04-11 14:38:40 +0300

    Update

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,10 @@ allFunctions.isWebp();
 // import Swiper, { Navigation, Pagination } from 'swiper';
 
 // const swiper = new Swiper();
+
+window.addEventListener('scroll', () => {
+	const header = document.getElementById('header').classList;
+	const activeClass = "header--scrolled";
+	if (scrollY > 80) header.add(activeClass)
+	else header.remove(activeClass)
+})
